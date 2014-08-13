a29a631f87033fe5c91ab16a30774d0d9d852422
Julio Faerman <jfaerman@gmail.com>
2014-08-13 16:46:22 -0300

    Get wildly tar ball

--- wildfly/recipes/setup.rb
+++ wildfly/recipes/setup.rb
@@ -1,6 +1,11 @@
-file "/tmp/hello-from-setup-recipe" do
+#Sanity Check
+file "/tmp/hello-setup-wildfly-1" do
   owner "root"
   group "root"
   mode "0755"
   action :create
+end
+
+execute "copy wildfly tarball from S3" do
+  command "aws s3 cp --region=sa-east-1 s3://dist-sa-east-1.craftware.com/WildFly/wildfly-8.1.0.Final.tar.gz /tmp/wildfly-8.1.0.Final.tar.gz"
 end
