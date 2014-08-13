b9ed19c7083ad6780925d9b3769282325ce1b78c
Julio Faerman <jfaerman@gmail.com>
2014-08-13 17:34:05 -0300

    Wildfly Setup

--- wildfly/recipes/setup.rb
+++ wildfly/recipes/setup.rb
@@ -1,11 +1,15 @@
-#Sanity Check
-file "/tmp/hello-setup-wildfly-1" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
+execute "sanity check" do
+  command "touch /tmp/sanity-check-wildfly-2"
 end
 
 execute "copy wildfly tarball from S3" do
   command "aws s3 cp --region=sa-east-1 s3://dist-sa-east-1.craftware.com/WildFly/wildfly-8.1.0.Final.tar.gz /tmp/wildfly-8.1.0.Final.tar.gz"
+end
+
+execute "uncompress wildfly" do
+  command "tar zxvf /tmp/wildfly-8.1.0.Final.tar.gz -C /opt"
+end
+
+execute "symlink" do
+  command "tar zxvf /tmp/wildfly-8.1.0.Final.tar.gz -C /opt"
 end
