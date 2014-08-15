ef9a6ad409ac524ed8d89e72b6add1f7dae1c5c7
Julio Faerman <jfaerman@gmail.com>
2014-08-15 19:53:41 -0300

    first stab at environment variables

--- kornell-api/recipes/setup.rb
+++ kornell-api/recipes/setup.rb
@@ -10,3 +10,26 @@ template 'kornell-api-container-configuration' do
   mode 0644
   backup false
 end
+
+ENV['JDBC_CONNECTION_STRING'] = "#{node[:kornell-api][:jdbc][:url]}"
+ENV['JDBC_DRIVER'] = "#{node[:kornell-api][:jdbc][:driver]}"
+ENV['JDBC_USERNAME'] = "#{node[:kornell-api][:jdbc][:username]}"
+ENV['JDBC_PASSWORD'] = "#{node[:kornell-api][:jdbc][:password]}"
+
+ENV['NEWRELIC_ENV'] = "#{node[:kornell-api][:newrelic][:environment]}"
+
+ENV['SMTP_HOST'] = "#{node[:kornell-api][:smtp][:host]}"
+ENV['SMTP_PORT'] = "#{node[:kornell-api][:smtp][:port]}"
+ENV['SMTP_USERNAME'] = "#{node[:kornell-api][:smtp][:username]}"
+ENV['SMTP_PASSWORD'] = "#{node[:kornell-api][:smtp][:password]}"
+ENV['REPLY_TO'] = "#{node[:kornell-api][:smtp][:reply-to]}"
+
+ENV['USER_CONTENT_BUCKET'] = "#{node[:kornell-api][:user-content]}"
+
+
+bash 'start-kornell-api' do
+  user 'root'
+  code <<-EOC
+    echo $JDBC_CONNECTION_STRING >> /tmp/sanity
+  EOC
+end
