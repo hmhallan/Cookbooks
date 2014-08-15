9ed5d9ea354c284793476158fa3d7c79071017af
Julio Faerman <jfaerman@gmail.com>
2014-08-15 16:40:59 -0300

    fixed notification targets

--- wildfly/recipes/setup.rb
+++ wildfly/recipes/setup.rb
@@ -3,19 +3,19 @@ log "Setup wildfly"
 execute "wildfly-get-package" do
   command "aws s3 cp --region=#{node[:wildfly][:bucket_region]} #{node[:wildfly][:package_object]} #{node[:wildfly][:package_tmp]}"
   not_if  { File.directory?('#{node[:wildfly][:home]}') }
-  notifies :run, 'execute[extract-wildfly]', :immediately
+  notifies :run, 'execute[wildfly-extract]', :immediately
 end
 
 execute "wildfly-extract" do
   command "tar zxvf #{node[:wildfly][:package_tmp]} -C #{node[:wildfly][:extract_dir]}"
   action :nothing
-  notifies :run, 'execute[create-symlink]', :immediately
+  notifies :run, 'execute[wildfly-symlink]', :immediately
 end
 
 execute "wildfly-symlink" do
   command "ln -s #{node[:wildfly][:extract_dir]}/#{node[:wildfly][:extract_name]} #{node[:wildfly][:home]}"
   action :nothing
-  notifies :run, 'execute[cleanup]', :immediately
+  notifies :run, 'execute[wildfly-cleanup]', :immediately
 end
 
 execute "wildfly-cleanup" do
