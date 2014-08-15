c0fa370eaef060dd6c69017366bc6cdc0a20bc59
Julio Faerman <jfaerman@gmail.com>
2014-08-15 14:37:10 -0300

    Resource chaining

--- wildfly/recipes/setup.rb
+++ wildfly/recipes/setup.rb
@@ -1,17 +1,24 @@
 log "ALL YOU NEED IS LOG"
 
-execute "copy wildfly tarball from S3" do
+execute "get-package-object" do
   command "aws s3 cp --region=#{node[:wildfly][:bucket_region]} #{node[:wildfly][:package_object]} #{node[:wildfly][:package_tmp]}"
+  not_if  { File.directory?('#{node[:wildfly][:home]}') }
+  notifies :run, 'execute[extract-wildfly]', :immediately
 end
 
-execute "uncompress wildfly" do
+execute "extract-wildfly" do
   command "tar zxvf #{node[:wildfly][:package_tmp]} -C #{node[:wildfly][:extract_dir]}"
+  action :nothing
+  notifies :run, 'execute[create-symlink]', :immediately
 end
 
-execute "create symlink" do
+execute "create-symlink" do
   command "ln -s #{node[:wildfly][:extract_dir]}/#{node[:wildfly][:extract_name]} #{node[:wildfly][:home]}"
+  action :nothing
+  notifies :run, 'execute[cleanup]', :immediately
 end
 
-file "#{node[:wildfly][:package_tmp]}" do
-  action :delete
+execute "cleanup" do
+  command "rm #{node[:wildfly][:package_tmp]}"
+  action :nothing
 end
