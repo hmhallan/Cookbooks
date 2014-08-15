f3368b5f7bfbb37f6724b349ec5025aae7e1847e
Julio Faerman <jfaerman@gmail.com>
2014-08-15 15:59:23 -0300

    Unterminated string fix

--- kornell-api/recipes/setup.rb
+++ kornell-api/recipes/setup.rb
@@ -1,2 +1,9 @@
 include_recipe 'wildfly::setup'
 include_recipe 'newrelic::setup'
+
+file "/tmp/kornell-api-setup" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
--- wildfly/attributes/wildfly.rb
+++ wildfly/attributes/wildfly.rb
@@ -3,4 +3,4 @@ default[:wildfly][:package_object] = 's3://dist-sa-east-1.craftware.com/WildFly/
 default[:wildfly][:package_tmp] = '/tmp/wildfly-8.1.0.Final.tar.gz'
 default[:wildfly][:extract_dir] = '/opt'
 default[:wildfly][:extract_name] = 'wildfly-8.1.0.Final'
-default[:wildfly][:home] = '/opt/wildfly
+default[:wildfly][:home] = '/opt/wildfly'
