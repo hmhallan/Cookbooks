946c6a1d85c84489502e5789a4325d3b70b3cf45
Julio Faerman <jfaerman@gmail.com>
2014-08-15 19:01:36 -0300

    First stab at templates

--- kornell-api/recipes/setup.rb
+++ kornell-api/recipes/setup.rb
@@ -1,3 +1,12 @@
 include_recipe 'wildfly::setup'
 include_recipe 'newrelic::setup'
 include_recipe 'mysql_jdbc::setup'
+
+template 'kornell-api-container-configuration' do
+  path ::File.join(node['wildfly']['home'], 'standalone-kornell-api.xml')
+  source 'standalone-kornell-api.xml.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  backup false
+end
