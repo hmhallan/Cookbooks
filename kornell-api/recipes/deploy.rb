ad2afafcbaaae534cc593032e8587027ec4023b0
Julio Faerman <jfaerman@gmail.com>
2014-08-16 17:17:10 -0300

    no hyphens

--- kornell-api/recipes/deploy.rb
+++ kornell-api/recipes/deploy.rb
@@ -1,5 +1,5 @@
 
 node[:deploy].each do |application, deploy|
   log("Default Deploy Steps #{application} ")
-  log("* deploy_to = #{node[:deploy]['kornell-api'][:deploy_to]}")
+  log("* deploy_to = #{node[:deploy][:kornell_api][:deploy_to]}")
 end
