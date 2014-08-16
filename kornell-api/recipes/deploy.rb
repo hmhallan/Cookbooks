7f73112dca46bd9f551dba2d3cfe021ec5072ac4
Julio Faerman <jfaerman@gmail.com>
2014-08-16 14:16:32 -0300

    Custom deploy

--- kornell-api/recipes/deploy.rb
+++ kornell-api/recipes/deploy.rb
@@ -1,22 +1,5 @@
-include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
   log("Default Deploy Steps #{application} ")
-
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
-
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
-
-  log("Custom Deploy Steps #{application} ")
-
-  current_dir = ::File.join(deploy[:deploy_to], 'current')
-  log("* Current dir #{current_dir} ")
-
+  log("* deploy_to = #{node[:deploy]['kornell-api'][:deploy_to]}")
 end
