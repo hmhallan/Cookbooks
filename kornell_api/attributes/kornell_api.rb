00fd3980d03bdb950651c3826a1e83701a4e4b34
Julio Faerman <jfaerman@gmail.com>
2014-08-16 18:40:10 -0300

    Default props for kornell_api

--- kornell_api/attributes/kornell_api.rb
+++ kornell_api/attributes/kornell_api.rb
@@ -1,3 +1,3 @@
-node[:kornell_api][:bucket_region] = "sa-east-1"
-node[:kornell_api][:package_object] = "s3://dist-sa-east-1.craftware.com/Kornell/kornell-api.war"
-node[:kornell_api][:deploy_package] = "/opt/wildfly/standalone/deployments/kornell-api.war"
+default[:kornell_api][:bucket_region] = "sa-east-1"
+default[:kornell_api][:package_object] = "s3://dist-sa-east-1.craftware.com/Kornell/kornell-api.war"
+default[:kornell_api][:deploy_package] = "/opt/wildfly/standalone/deployments/kornell-api.war"
