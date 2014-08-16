8add2a7b9e90ac99dcf473d06a9f258cd794ff0f
Julio Faerman <jfaerman@gmail.com>
2014-08-16 18:55:21 -0300

    Fixed naming collision

--- kornell_api/recipes/deploy.rb
+++ kornell_api/recipes/deploy.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |application, deploy|
   log("** Deploying #{application} ")
 
-  execute "wildfly-get-package" do
+  execute "kornell_api-get-package" do
     command "aws s3 cp --region=#{node[:kornell_api][:bucket_region]} #{node[:kornell_api][:package_object]} #{node[:kornell_api][:deploy_package]}"
   end
 end
