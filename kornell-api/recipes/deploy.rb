453c6968ccbc55737580c76bcefcb0dfef90e81f
Julio Faerman <jfaerman@gmail.com>
2014-08-16 13:49:40 -0300

    First stab at deploy

--- kornell-api/recipes/deploy.rb
+++ kornell-api/recipes/deploy.rb
@@ -1,3 +1,22 @@
+include_recipe 'deploy'
+
 node[:deploy].each do |application, deploy|
-  log("Deploying Application #{application} ")
+  log("Default Deploy Steps #{application} ")
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  log("Custom Deploy Steps #{application} ")
+
+  current_dir = ::File.join(deploy[:deploy_to], 'current')
+  log("* Current dir #{current_dir} ")
+
 end
