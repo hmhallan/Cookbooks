5d4609c270a0f2ef559ec48c666369257e8dfa6b
Julio Faerman <jfaerman@gmail.com>
2014-08-17 17:18:54 -0300

    WIP Deploy

--- kornell_api/recipes/deploy.rb
+++ kornell_api/recipes/deploy.rb
@@ -1,24 +1,16 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
-
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
-
-
-#  tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
-#  directory("/var/log/stab/")
-#  log("stab-deploy [#{application}] ")
-#  file "/var/log/stab/stab-deploy-#{tstamp}"
+  tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+  log("stab-deploy [#{application}] ")
+  directory("/var/log/stab/")
+  file "/var/log/stab/stab-deploy-#{tstamp}"
 
   execute "kornell_api-get-package" do
     command "aws s3 cp --region=#{node[:kornell_api][:bucket_region]} #{node[:kornell_api][:package_object]} #{node[:kornell_api][:deploy_package]}"
   end
+end
+
+service "wildfly" do
+  action :restart
 end
