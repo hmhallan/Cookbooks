9c953bf62df4ce6efb584e1896f28c72c5ccae92
Julio Faerman <jfaerman@gmail.com>
2014-08-17 13:26:48 -0300

    WIP Deploy

--- kornell_api/recipes/deploy.rb
+++ kornell_api/recipes/deploy.rb
@@ -1,5 +1,8 @@
 node[:deploy].each do |application, deploy|
-  log("** Deploying #{application} ")
+  tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+  directory("/var/log/stab/")
+  log("stab-deploy [#{application}] ")
+  file "/var/log/stab/stab-deploy-#{tstamp}"
 
   execute "kornell_api-get-package" do
     command "aws s3 cp --region=#{node[:kornell_api][:bucket_region]} #{node[:kornell_api][:package_object]} #{node[:kornell_api][:deploy_package]}"
--- kornell_api/recipes/setup.rb
+++ kornell_api/recipes/setup.rb
@@ -1,3 +1,9 @@
+tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+directory("/var/log/stab/")
+log("stab-setup-#{tstamp}")
+
+file "/var/log/stab/stab-setup-#{tstamp}"
+
 include_recipe 'wildfly::setup'
 include_recipe 'newrelic::setup'
 include_recipe 'mysql_jdbc::setup'
@@ -12,31 +18,3 @@ template 'kornell-api-container-configuration' do
 end
 
 include_recipe 'wildfly::service'
-
-# log("Setting Envirnment")
-
-# ENV['JDBC_USERNAME'] = "#{node[:kornell_api][:jdbc][:username]}"
-# ENV['JDBC_PASSWORD'] = "#{node[:kornell_api][:jdbc][:password]}"
-# 
-# ENV['SMTP_HOST'] = "#{node[:kornell_api][:smtp][:host]}"
-# ENV['SMTP_PORT'] = "#{node[:kornell_api][:smtp][:port]}"
-# ENV['SMTP_USERNAME'] = "#{node[:kornell_api][:smtp][:username]}"
-# ENV['SMTP_PASSWORD'] = "#{node[:kornell_api][:smtp][:password]}"
-# ENV['REPLY_TO'] = "#{node[:kornell_api][:smtp][:reply_to]}"
-# 
-# ENV['USER_CONTENT_BUCKET'] = "#{node[:kornell_api][:user_content]}"
-# ENV['JAVA_OPTS'] = " -javaagent:/opt/newrelic/newrelic.jar"
-# 
-# log("Starting Kornell API")
-# 
-# wildfly_cmd = "/opt/wildfly/bin/standalone.sh"
-# wildfly_cmd <<= " -c standalone-kornell-api.xml"
-# wildfly_cmd <<= " -b 0.0.0.0"
-# wildfly_cmd <<= " -Dnewrelic.environment=#{node[:kornell_api][:newrelic][:environment]}"
-# wildfly_cmd <<= ' -DJNDI_ROOT="java:/"'
-# wildfly_cmd <<= ' -DJNDI_DATASOURCE="datasources/KornellDS"'
-# wildfly_cmd <<= " -Dkornell.api.jdbc.url=#{node[:kornell_api][:jdbc][:url]}"
-# wildfly_cmd <<= " -Dkornell.api.jdbc.driver=#{node[:kornell_api][:jdbc][:driver]}"
-# wildfly_cmd <<= " -Dkornell.api.jdbc.password=$JDBC_PASSWORD"
-# wildfly_cmd <<= " -Dkornell.api.jdbc.username=$JDBC_USERNAME"
-# wildfly_cmd <<= " &"
