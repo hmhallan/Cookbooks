2a7c7bd044b6366355efbf84fa31ac181595cfd6
Julio Faerman <jfaerman@gmail.com>
2014-08-17 14:07:47 -0300

    WIP Deploy

--- kornell_api/recipes/deploy.rb
+++ kornell_api/recipes/deploy.rb
@@ -1,4 +1,17 @@
+include_recipe 'deploy'
+
 node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+#    user deploy[:user]
+#    group deploy[:group]
+#    path deploy[:deploy_to]
+  end
+
+#  opsworks_deploy do
+#    deploy_data deploy
+#    app application
+#  end
+
   tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
   directory("/var/log/stab/")
   log("stab-deploy [#{application}] ")
