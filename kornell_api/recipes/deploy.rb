fb531c82fa8e6eb61de2eb9c8d8ef3496798d634
Julio Faerman <jfaerman@gmail.com>
2014-08-17 16:23:20 -0300

    Ignore debug

--- kornell_api/recipes/deploy.rb
+++ kornell_api/recipes/deploy.rb
@@ -12,10 +12,11 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
-  directory("/var/log/stab/")
-  log("stab-deploy [#{application}] ")
-  file "/var/log/stab/stab-deploy-#{tstamp}"
+
+#  tstamp = ""+Time.now.strftime("%Y-%m-%d-%H-%M-%S")
+#  directory("/var/log/stab/")
+#  log("stab-deploy [#{application}] ")
+#  file "/var/log/stab/stab-deploy-#{tstamp}"
 
   execute "kornell_api-get-package" do
     command "aws s3 cp --region=#{node[:kornell_api][:bucket_region]} #{node[:kornell_api][:package_object]} #{node[:kornell_api][:deploy_package]}"
