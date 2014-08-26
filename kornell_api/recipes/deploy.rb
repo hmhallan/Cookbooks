58a260b940ba0b8029b3b074f00e4abfbf41fa7f
Julio Faerman <jfaerman@gmail.com>
2014-08-26 16:54:24 -0300

    deployment wip

--- kornell_api/recipes/deploy.rb
+++ kornell_api/recipes/deploy.rb
@@ -9,6 +9,10 @@ node[:deploy].each do |application, deploy|
   execute "kornell_api-get-package" do
     command "aws s3 cp --region=#{node[:kornell_api][:bucket_region]} #{node[:kornell_api][:package_object]} #{node[:kornell_api][:deploy_package]}"
   end
+
+  file "#{node[:kornell_api][:deploy_package]}.dodeploy" do
+    action :create
+  end
 end
 
 service "wildfly" do
