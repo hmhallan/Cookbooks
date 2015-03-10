17bf5035c4dbade6871f38b10273a3354abdaf02
arislaw <aridiasneto@gmail.com>
2015-03-10 08:14:24 -0300

    Update wildfly.rb

--- wildfly/attributes/wildfly.rb
+++ wildfly/attributes/wildfly.rb
@@ -1,5 +1,5 @@
 default[:wildfly][:bucket_region] = 'sa-east-1'
-default[:wildfly][:package_object] = 's3://dist-sa-east-1.craftware.com/WildFly/wildfly-8.1.0.Final.tar.gz'
+default[:wildfly][:package_object] = 'http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.zip'
 default[:wildfly][:package_tmp] = '/tmp/wildfly-8.1.0.Final.tar.gz'
 default[:wildfly][:extract_dir] = '/opt'
 default[:wildfly][:extract_name] = 'wildfly-8.1.0.Final'
